a8cf84d0ca2517262bb8909d7f4560bb491c26d7
Vishnu V S <vishnusenthil222@gmail.com>
2024-03-03 16:36:18 +0530

    dashboard sidebar footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div>
       <Navbar />
       <section className="flex">
-        <nav className="bg-[#212529] w-[225px] h-screen">
+        <nav className="bg-[#212529] relative w-[225px] h-[92vh]">
           <div className="w-full h-[58px] px-4 pt-7 pb-3 text-[#585b5e] font-medium text-[13px]">
             CORE
           </div>
@@ -26,6 +26,11 @@ function Home() {
             <MdOutlineCreateNewFolder />
             <p className="hover:text-white">Create User</p>
           </Link>
+
+          <div className="text-[#818588] bg-[#343a40] absolute bottom-0 flex flex-col w-full h-[67.2px] p-3">
+            <p className="font-medium">Logged in as:</p>
+            <h2>Admin</h2>
+          </div>
         </nav>
         <section className="px-6 flex-1">
           <p className="text-[35px] mb-[.5rem] mt-[1.5rem] text-[#212529] leading-[1.2]">

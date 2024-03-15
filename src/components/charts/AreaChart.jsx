87e30a0e7651dcecaa6ee5eabe1e53a86d1d53ee
Vishnu V S <vishnusenthil222@gmail.com>
2024-03-15 09:25:38 +0530

    chart from backend

--- src/components/charts/AreaChart.jsx
+++ src/components/charts/AreaChart.jsx
@@ -1,5 +1,5 @@
-// import "./styles.css";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
   AreaChart,
   Area,
@@ -8,60 +8,28 @@ import {
   CartesianGrid,
   Tooltip,
 } from "recharts";
+import areaChart from '../../data/areaData.json'
 
-const data = [
-  {
-    name: " A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: " B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: " C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: " D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: " E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: " F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: " G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 export default function App() {
+  const [areaChartData, setAreaChartData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/areaData.json")
+      .then((res) => setAreaChartData(res.data))
+      .catch((err) => console.error(err.message));
+  }, []);
+
   return (
     <div className="border md:w-[500px] flex flex-col items-center">
-      <h2 className="p-3 border w-full text-center mb-3 bg-[#f7f7f7]">Area Chart</h2>
+      <h2 className="p-3 border w-full text-center mb-3 bg-[#f7f7f7]">
+        Area Chart
+      </h2>
       <AreaChart
         width={450}
         height={320}
-        data={data}
+        data={areaChartData}
         margin={{
           top: 10,
           right: 30,
@@ -73,7 +41,20 @@ export default function App() {
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#007bff" />
+        <Area
+          type="monotone"
+          dataKey="uv"
+          stackId="1"
+          stroke="#8884d8"
+          fill="#007bff"
+        />
+        <Area
+          type="monotone"
+          dataKey="pv"
+          stackId="1"
+          stroke="#82ca9d"
+          fill="gray"
+        />
       </AreaChart>
     </div>
   );
--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -1,62 +1,26 @@
-// import "./styles.css";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
-  AreaChart,
   Area,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
 } from "recharts";
-
-const data = [
-  {
-    name: "A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import barChart from "../../data/barData.json";
 
 export default function App() {
+  const [barChartData, setBarChartData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/barData.json")
+      .then((res) => setBarChartData(res.data))
+      .catch((err) => console.error(err.message));
+  }, []);
+
   return (
     <div className="border md:w-[500px] flex flex-col items-center">
       <h2 className="p-3 border w-full text-center mb-3 bg-[#f7f7f7]">
@@ -65,7 +29,7 @@ export default function App() {
       <BarChart
         width={450}
         height={320}
-        data={data}
+        data={barChartData}
         margin={{
           top: 10,
           right: 30,
